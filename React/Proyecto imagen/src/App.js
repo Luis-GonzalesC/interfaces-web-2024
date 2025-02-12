6ff72e6372c4478d39475e38024424243aa208e1
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2025-02-12 23:09:09 +0100

    practicando react

--- React/Proyecto imagen/src/App.js
+++ React/Proyecto imagen/src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Galeria from './Galeria.js';
 import Texto from './Texto.js';
 
 function App() {
+  const [valorinput, setvalorInput] = useState();
+  
+  const cambio = (e)=>{
+    setvalorInput(e.target.value);
+  }
+  
+
   return (
     <>
-      <Galeria/>
-      <Texto/>
+      {/*<Galeria/>
+      <Texto/>*/}
+
+        <input onChange={cambio} type='text' name='si' value={valorinput}/>
+        <p>{valorinput}</p>
+
     </>
   );
 }
